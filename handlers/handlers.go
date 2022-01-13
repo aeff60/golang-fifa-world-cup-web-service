056c54c9485ed3e02c88ccc19f38836c6baf02db
Sirasit Boonklang <ssbunklang@gmail.com>
2022-01-14 01:50:19 +0700

    Complete Module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,9 +14,8 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
+	year := req.URL.Query().Get("year")
 
-	req.URL.Query().Get("year")
-	year := "year"
 	if year == "" {
 		winners, err := data.ListAllJSON()
 		if err != nil {
@@ -27,32 +26,18 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 	} else {
 		filteredWinners, err := data.ListAllByYear(year)
 		if err != nil {
-
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
-
 		res.Write(filteredWinners)
+
 	}
 
 }
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-	accessToken := req.Header.Get("X-ACCESS-TOKEN")
-	isTokenValid := data.IsAccessTokenValid(accessToken)
-	if !isTokenValid {
-		res.WriteHeader(http.StatusUnauthorized)
 
-	} else {
-
-		err := data.AddNewWinner(req.Body)
-		if err != nil {
-			res.WriteHeader(http.StatusUnprocessableEntity) //422
-			return
-		}
-		res.WriteHeader(http.StatusCreated)
-	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
